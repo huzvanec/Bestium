702365cc8649247985253c53d09d5157ea6d905a
Hužva <huzvanec@email.cz>
2025-07-31 21:24:29 +0200

    fix(core): spawn initialization running every time

--- core/src/main/kotlin/cz/jeme/bestium/BestiumEntityManagerImpl.kt
+++ core/src/main/kotlin/cz/jeme/bestium/BestiumEntityManagerImpl.kt
@@ -53,76 +53,77 @@ object BestiumEntityManagerImpl : BestiumEntityManager {
 
         val key = injection.key // the key of the Bestium entity
         val entity = injectable.asBestiumEntity() // the injectable as a NMS entity
-        val bukkitEntity = entity.bukkitEntity // so we can use Bukkit API
-
-        // true if this entity is spawning for the first time
-        val isFirstSpawn = !PersistentData.BESTIUM_ID.check(bukkitEntity)
-
-        // save Bestium entity key
-        // runs regardless of whether the entity already has the key stored
-        // ensures that it cannot be tampered with as much
-        PersistentData.BESTIUM_ID[bukkitEntity] = key
-
-        /** Picks a variant for the entity, returns `null` if no model should be applied */
-        fun pickVariant(): BoundEntityVariant? {
-            val variant = injection.variantPicker.pick(
-                injection.variants,
-                VariantPicker.Context(
-                    entity,
-                    entityType,
-                    injection
-                )
-            )
-
-            variant?.let {
-                // a variant should be applied, check that it's valid
-
-                val variantEntityKey = variant.injection.key
-                if (variantEntityKey != key) {
-                    // the picked variant does not belong to the entity spawning, but to a different entity
-                    throw IllegalStateException(
-                        "Provided entity variant is registered for a different entity: ${variantEntityKey.asString()} instead of ${key.asString()}"
-                    )
-                }
-            }
-
-            return variant
-        }
-
 
         // save a snapshot of worldsLoaded when the mob spawns
         val worldsLoaded = WorldLoadListener.worldsLoaded
 
-        /**
-         * Attempts to apply a model to the entity, returns `true` if the
-         * model was applied, if BetterModel isn't loaded, returns `false`
-         *
-         * Will throw an exception if BetterModel is not loaded.
-         */
-        fun applyModel(modelName: String) {
-            if (worldsLoaded) {
-                // if worlds were loaded when the entity initially spawned
-                // use normal BM API
-                BetterModel.model(modelName).map { renderer ->
-                    renderer.getOrCreate(bukkitEntity)
-                }
-            } else {
-                // if worlds were not loaded (= the entity spawned while worlds were being loaded),
-                // BM was not enabled yet and it cannot correctly handle model tracking
-                // a workaround for this is to directly save the BM model data to this entity
-                // in legacy formatting and wait for BM to pick the entity up and migrate it
-                bukkitEntity.persistentDataContainer.set(
-                    EntityTrackerRegistry.TRACKING_ID,
-                    PersistentDataType.STRING,
-                    modelName
-                )
-            }
-        }
+        // basically everything here gets delayed by 1 tick
+        // when Bukkit gets correctly initialized
+        fun lateInitializeBestiumEntity() {
+            val bukkitEntity = entity.bukkitEntity // so we can use Bukkit API
 
-        if (isFirstSpawn) {
-            // delay the variant picking logic by 1 tick so the entity
-            // is properly initialized          
-            Bukkit.getScheduler().runTask(BestiumPlugin) { _ ->
+            // true if this entity is spawning for the first time
+            val isFirstSpawn = !PersistentData.BESTIUM_ID.check(bukkitEntity)
+
+            // save Bestium entity key
+            // runs regardless of whether the entity already has the key stored
+            // ensures that it cannot be tampered with as much
+            PersistentData.BESTIUM_ID[bukkitEntity] = key
+
+            /** Picks a variant for the entity, returns `null` if no model should be applied */
+            fun pickVariant(): BoundEntityVariant? {
+                val variant = injection.variantPicker.pick(
+                    injection.variants,
+                    VariantPicker.Context(
+                        entity,
+                        entityType,
+                        injection
+                    )
+                )
+
+                variant?.let {
+                    // a variant should be applied, check that it's valid
+
+                    val variantEntityKey = variant.injection.key
+                    if (variantEntityKey != key) {
+                        // the picked variant does not belong to the entity spawning, but to a different entity
+                        throw IllegalStateException(
+                            "Provided entity variant is registered for a different entity: ${variantEntityKey.asString()} instead of ${key.asString()}"
+                        )
+                    }
+                }
+
+                return variant
+            }
+
+            /**
+             * Attempts to apply a model to the entity, returns `true` if the
+             * model was applied, if BetterModel isn't loaded, returns `false`
+             *
+             * Will throw an exception if BetterModel is not loaded.
+             */
+            fun applyModel(modelName: String) {
+                if (worldsLoaded) {
+                    // if worlds were loaded when the entity initially spawned
+                    // use normal BM API
+                    BetterModel.model(modelName).map { renderer ->
+                        renderer.getOrCreate(bukkitEntity)
+                    }
+                } else {
+                    // if worlds were not loaded (= the entity spawned while worlds were being loaded),
+                    // BM was not enabled yet and it cannot correctly handle model tracking
+                    // a workaround for this is to directly save the BM model data to this entity
+                    // in legacy formatting and wait for BM to pick the entity up and migrate it
+                    bukkitEntity.persistentDataContainer.set(
+                        EntityTrackerRegistry.TRACKING_ID,
+                        PersistentDataType.STRING,
+                        modelName
+                    )
+                }
+            }
+
+            if (isFirstSpawn) {
+                println("for the first time: ${bukkitEntity.uniqueId} > ${bukkitEntity.x} / ${bukkitEntity.y} / ${bukkitEntity.z}")
                 val variant = pickVariant()
                 PersistentData.BESTIUM_VARIANT[bukkitEntity] = (variant?.id ?: "NONE") // save variant data
 
@@ -137,20 +138,22 @@ object BestiumEntityManagerImpl : BestiumEntityManager {
                         PersistentData.BESTIUM_PENDING_MODEL[bukkitEntity] = variant.modelName
                     }
                 }
-            }
 
-            // store current data version
-            PersistentData.BESTIUM_DATA_VERSION[bukkitEntity] = CURRENT_DATA_VERSION
-        } else { // the entity was already spawned and is just being loaded
-            migrate(bukkitEntity, entityType)
+                // store current data version
+                PersistentData.BESTIUM_DATA_VERSION[bukkitEntity] = CURRENT_DATA_VERSION
+            } else { // the entity was already spawned and is just being loaded
+                migrate(bukkitEntity, entityType)
 
-            if (PluginSupportImpl.betterModel()) {
-                // better model is loaded check for a pending model
-                PersistentData.BESTIUM_PENDING_MODEL[bukkitEntity]?.let {
-                    applyModel(it)
+                if (PluginSupportImpl.betterModel()) {
+                    // better model is loaded check for a pending model
+                    PersistentData.BESTIUM_PENDING_MODEL[bukkitEntity]?.let {
+                        applyModel(it)
+                    }
                 }
             }
         }
+
+        Bukkit.getScheduler().runTask(BestiumPlugin, ::lateInitializeBestiumEntity)
     }
 
     override fun saveBestiumEntity(injectable: Injectable, output: ValueOutput) {
